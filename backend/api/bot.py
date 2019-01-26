f7edf011cfeefb3891e6c3890ba47e58559458db
QuickSilver <avataraang399@gmail.com>
2019-01-27 06:46:33 +1000

    Fix bot #2

--- backend/api/bot.py
+++ backend/api/bot.py
@@ -8,22 +8,3 @@ from django.conf import settings
 
 telebot.apihelper.proxy = settings.KAPPA_PROXY
 KappaBot = telebot.TeleBot(settings.KAPPA_TOKEN)
-
-
-@KappaBot.message_handler(func=lambda message: True, content_types=['text'])
-def process_sql(message):
-    data = kira.connect().cursor().execute(message.text).fetch()
-    try:
-        response = json.loads(data)
-        if 'columns' in response['result']:
-            response = tabulate(
-                response['result']['records'],
-                [x['name'] for x in response['result']['columns']],
-                tablefmt='presto',
-                numalign='None'
-            )
-        else:
-            response = json.dumps(response['result'])
-    except:
-        response = data
-    KappaBot.reply_to(message, response)
--- backend/urls.py
+++ backend/urls.py
@@ -6,13 +6,33 @@ from backend.api.bot import KappaBot
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('query/', views.QueryView.as_view()),
+    path('query', views.QueryView.as_view()),
     path(settings.KAPPA_WEBHOOK, views.BotView.as_view()),
     path('', views.AppView.as_view()),
 ]
 
-KappaBot.remove_webhook()
 if (settings.KAPPA_WEBHOOK is not None):
+    print(settings.KAPPA_WEBHOOK_URL + settings.KAPPA_WEBHOOK)
+    KappaBot.remove_webhook()
     KappaBot.set_webhook(
         url=settings.KAPPA_WEBHOOK_URL + settings.KAPPA_WEBHOOK
     ) # certificate=open(settings.KAPPA_WEBHOOK_CERT, 'r')
+
+
+@KappaBot.message_handler(func=lambda message: True, content_types=['text'])
+def process_sql(message):
+    data = kira.connect().cursor().execute(message.text).fetch()
+    try:
+        response = json.loads(data)
+        if 'columns' in response['result']:
+            response = tabulate(
+                response['result']['records'],
+                [x['name'] for x in response['result']['columns']],
+                tablefmt='presto',
+                numalign='None'
+            )
+        else:
+            response = json.dumps(response['result'])
+    except:
+        response = data
+    KappaBot.reply_to(message, response)
