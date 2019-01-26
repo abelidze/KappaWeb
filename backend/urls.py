3ab60f1c547619e83fdf5fd59ff1c3d3b5e58ef7
QuickSilver <avataraang399@gmail.com>
2019-01-27 05:57:39 +1000

    Fix bot #1

--- backend/urls.py
+++ backend/urls.py
@@ -6,13 +6,13 @@ from backend.api.bot import KappaBot
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('query', views.QueryView.as_view()),
+    path('query/', views.QueryView.as_view()),
+    path(settings.KAPPA_WEBHOOK, views.BotView.as_view()),
     path('', views.AppView.as_view()),
 ]
 
 KappaBot.remove_webhook()
 if (settings.KAPPA_WEBHOOK is not None):
-    urlpatterns.append( path(settings.KAPPA_WEBHOOK, views.BotView.as_view()) )
     KappaBot.set_webhook(
         url=settings.KAPPA_WEBHOOK_URL + settings.KAPPA_WEBHOOK
     ) # certificate=open(settings.KAPPA_WEBHOOK_CERT, 'r')
