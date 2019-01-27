133acd59df614ba5b416f506a9fe273d759704b1
Quicksilver <avataraang399@gmail.com>
2019-01-27 05:12:11 +0000

    Typo

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -10,7 +10,8 @@ class ApiConfig(AppConfig):
         if (settings.KAPPA_WEBHOOK is not None):
             print(KappaBot.remove_webhook())
             print(KappaBot.set_webhook(
-                url=settings.KAPPA_WEBHOOK_URL + settings.KAPPA_WEBHOOK
-            ) )# certificate=open(settings.KAPPA_WEBHOOK_CERT, 'r')
+                url=settings.KAPPA_WEBHOOK_URL + settings.KAPPA_WEBHOOK,
+                certificate=open('/etc/letsencrypt/live/kappa.skillmasters.ga/fullchain.pem', 'r')
+            ))
             print(KappaBot.get_webhook_info())
 
